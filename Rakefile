463e8250be6693df4743bd211eef8f74b36462b4
omer91se <omer.segal@fiverr.com>
2019-08-11 12:35:04 +0300

    merged

--- Rakefile
+++ Rakefile
@@ -4,4 +4,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-SegalAgriculture::Application.load_tasks
+SampleApp::Application.load_tasks
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SegalAgriculture::Application.config.secret_token = 'cd349d1a690ff2750c9aad62b38381133f6a16f5b2dd03780c4c7e1eb92fa1ff2b93ba9064bfcaca47dc758fe3424ca1fe207a58058f6b3a8572e6ec45d8492b'
+SampleApp::Application.config.secret_token = 'f5bc5d6a293923a34c0bb0533604bc4c24e032e29afd5585e6a313c1b4fc59c9341a662c07fa7dbfd7716128ceeae85e56e846c2b88302722df0d7d666bae791'
