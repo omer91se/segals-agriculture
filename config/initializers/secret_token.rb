dbd795a760813df299c8e677ababa69166c076a7
omer91se <omer.segal@fiverr.com>
2019-08-11 12:31:13 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SegalAgriculture::Application.config.secret_token = 'cd349d1a690ff2750c9aad62b38381133f6a16f5b2dd03780c4c7e1eb92fa1ff2b93ba9064bfcaca47dc758fe3424ca1fe207a58058f6b3a8572e6ec45d8492b'
